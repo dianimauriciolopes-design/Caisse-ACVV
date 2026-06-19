91c61476e88b1b8c4f9721e0c5814b431e5ec2bf
Diani Mauricio-Lopes <dianimauriciolopes@gmail.com>
2026-06-19 17:40:43 +0200

    fix

--- scripts/init_prod_db.py
+++ scripts/init_prod_db.py
@@ -1,3 +1,11 @@
+import sys, os
+
+# Ajouter le dossier parent (/app) au PYTHONPATH
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
+
 from database import init_db
+
+print("Initialisation de la base Railway…")
 init_db()
-print("Base de données initialisée sur Railway.")
+print("✔ Base initialisée avec succès !")
